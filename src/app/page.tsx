7478c53398bc90925e71a7c2d1799c2d8bc89cca
sabari245 <sabari2004@hotmail.com>
2024-01-22 19:35:38 +0530

    updated the colors in hero sections:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,19 +27,20 @@ const Home = () => {
       <SeoMeta />
       <section className="section pt-14">
         <div className="container">
-          <div className="row justify-center">
+          <div className="row justify-center mt-10">
             <div className="mb-16 text-center lg:col-7">
-            <h1
-              className="head1 mb-4 text-5xl font-bold"
-              dangerouslySetInnerHTML={markdownify(banner.title)}
-            />
+              <h1
+                className="head1 mb-4 text-5xl font-bold"
+                style={{ background: "linear-gradient(to right, #f57114, #5e2591)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent" }}
+                dangerouslySetInnerHTML={markdownify(banner.title)}
+              />
 
               <p
                 className="mb-8"
                 dangerouslySetInnerHTML={markdownify(banner.content ?? "")}
               />
               {banner.button!.enable && (
-                <Link className="btn btn-primary" href={banner.button!.link}>
+                <Link className="btn btn-primary" href={banner.button!.link} style={{ background: "linear-gradient(to right, #f57114, #5e2591)", outline: "none", border: "none", color: "white", padding: "0.75rem 2.5rem" }}>
                   {banner.button!.label}
                 </Link>
               )}
@@ -68,9 +69,8 @@ const Home = () => {
           <div className="container">
             <div className="row items-center justify-between">
               <div
-                className={`mb:md-0 mb-6 md:col-5 ${
-                  index % 2 !== 0 && "md:order-2"
-                }`}
+                className={`mb:md-0 mb-6 md:col-5 ${index % 2 !== 0 && "md:order-2"
+                  }`}
               >
                 <ImageFallback
                   src={feature.image}
@@ -80,9 +80,8 @@ const Home = () => {
                 />
               </div>
               <div
-                className={`md:col-7 lg:col-6 ${
-                  index % 2 !== 0 && "md:order-1"
-                }`}
+                className={`md:col-7 lg:col-6 ${index % 2 !== 0 && "md:order-1"
+                  }`}
               >
                 <h2
                   className="mb-4"
