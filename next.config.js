64fa806a983036e3b189f7dad65490f23d8aa369
RahulDev2004 <talktorahul.b@gmail.com>
2024-04-16 18:46:46 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   basePath: config.base_path !== "/" ? config.base_path : "",
   trailingSlash: config.site.trailing_slash,
   output: 'standalone',
+  async rewrites() {
+    return [
+        {
+          source: '/grants',
+          destination: 'https://mpb-form-iuhnubeuyce7ec677rc32786278b7z28nseven.vercel.app',
+        }
+    ]
+}
 };
 
 module.exports = nextConfig;
