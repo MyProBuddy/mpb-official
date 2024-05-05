41020cfa59a6e5e201c8da91302a4ee08135c276
RahulDev2004 <talktorahul.b@gmail.com>
2024-05-05 23:49:13 +0530

    added how it works page

--- src/app/howitworks/page.tsx
+++ src/app/howitworks/page.tsx
@@ -13,7 +13,7 @@ const Howitworks: React.FC = () => {
         <ul>
             <li>
                 <h3 className={styles.heading}>STAGE 1</h3>
-                <p className={styles.p}>Fill the form to check the eligibity
+                <p className={styles.p}>Fill the form to check the eligibity.
  </p>
                
                 {/* <span className={styles.date}>January 2021</span> */}
@@ -37,7 +37,7 @@ const Howitworks: React.FC = () => {
             </li>
             <li>
                 <h3 className={styles.heading}>STAGE 4</h3>
-                <p className={styles.p}>Get a call from our expects if your idea is shortlisted
+                <p className={styles.p}>Get a call from our expects if your idea is shortlisted.
  </p>
                 
                 {/* <span className={styles.date}>April 2021</span> */}
@@ -47,7 +47,7 @@ const Howitworks: React.FC = () => {
             
             <li>
                 <h3 className={styles.heading}>STAGE 5</h3>
-                <p className={styles.p}>Pay  ‘2499’ to unlock your grant report 
+                <p className={styles.p}>Pay  ‘2499’ to unlock your grant report.
 
  </p>
                 
@@ -56,7 +56,7 @@ const Howitworks: React.FC = () => {
             </li>
             <li>
                 <h3 className={styles.heading}>STAGE 6</h3>
-                <p className={styles.p}>Report delivery along with gpt (grant proposal template) and calendly link to select slot for the one to one consultation call
+                <p className={styles.p}>Report delivery along with gpt (grant proposal template) and calendly link to select slot for the one to one consultation call.
 
  </p>
                 
